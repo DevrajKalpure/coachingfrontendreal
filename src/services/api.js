326d1e7c36d24581158fdacc3252b367096852c4
Devraj Kalpure <150429393+DevrajKalpure@users.noreply.github.com>
2025-08-29 10:38:15 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.jstechnohub.com/api';             
+const API_URL = 'https://coachingwebsite-production.up.railway.app/api';             
 //'http://16.171.144.29:8080/api';
 
 const api = axios.create({
